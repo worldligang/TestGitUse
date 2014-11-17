55b900a89070456a2a7a03cccb1a5bf1ff1d40d1
ligang <worldligang@163.com>
2014-11-17 14:42:36 +0800

    bug 3

--- GitDemo/AppDelegate.m
+++ GitDemo/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Override point for customization after application launch.   bug 3
     return YES;
 }
 
